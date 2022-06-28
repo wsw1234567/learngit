937bfa05f6b4a7c4a933f7e6e4a077d6ee4b70a1
wsw1234567 <w13857097316@163.com>
2022-06-28 15:48:07 +0800

    作业2

---
+++ homework2/example2.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "script starting"
+
+foo(){
+    echo "Function foo is executing"
+}
+
+foo
+
+echo "script ended"
+
+exit 0
---
+++ homework2/example3.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+foo(){
+    echo $1
+}
+
+echo $1
+
+foo 2
+
+echo $1
+
+exit 0
